abbf4aa7c489903e90061f074e071907e2cc0038
sthennin <henning.stahlberg@epfl.ch>
2026-04-10 09:35:51 +0200

    cosmetics only

--- ftwindow_paint.cpp
+++ ftwindow_paint.cpp
@@ -1489,10 +1489,16 @@ void FtWindow::paintEvent(QPaintEvent *)
             }
             p.setPen(QColor(200, 200, 200));
             QFont lf; lf.setPixelSize(28); p.setFont(lf);
-            if (m_displayMode == 3)
+            if (m_displayMode == 3) {
                 p.drawText(frame.x(), frame.y() - 4, "Powerspectrum");
-            else
+                QFontMetrics lfm(lf);
+                QFont sf; sf.setPixelSize(14); p.setFont(sf);
+                p.drawText(frame.x() + lfm.horizontalAdvance("Powerspectrum") + 4,
+                           frame.y() - 4, "displayed as log(1 + amp*amp)");
+                p.setFont(lf);
+            } else {
                 p.drawText(frame.x(), frame.y() - 4, "Complex Fourier Transform");
+            }
 
             const QImage &img = (m_displayMode == 3) ? m_powerImg : m_complexImg;
             drawImageWithFrame(p, frame, img, m_zoom[1], m_fftN, m_fftN);
@@ -1637,6 +1643,13 @@ void FtWindow::paintEvent(QPaintEvent *)
             lf.setPixelSize(28);
             p.setFont(lf);
             p.drawText(frame1.x(), frame1.y() - 4, label1);
+            if (m_displayMode == 1) {
+                QFontMetrics lfm(lf);
+                QFont sf; sf.setPixelSize(14); p.setFont(sf);
+                p.drawText(frame1.x() + lfm.horizontalAdvance(label1) + 4,
+                           frame1.y() - 4, "displayed as log(1+amp)");
+                p.setFont(lf);
+            }
             p.drawText(frame2.x(), frame2.y() - 4, label2);
 
             drawImageWithFrame(p, frame1, *img1, m_zoom[1], m_fftN, m_fftN);
