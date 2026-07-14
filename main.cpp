90b2fd583a15e5019d758184bebe8fd4ed0cd830
sthennin <henning.stahlberg@epfl.ch>
2026-07-14 16:45:01 +0200

    open does not load big images

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <QApplication>
-#include <QTimer>
-#include <QPixmap>
 #include "ftwindow.h"
 
 int main(int argc, char *argv[])
@@ -9,16 +7,6 @@ int main(int argc, char *argv[])
 
     FtWindow w;
     w.show();
-
-    // TEMPORARY screenshot hook (to be reverted)
-    QByteArray shot = qgetenv("FT_SHOT");
-    if (!shot.isEmpty()) {
-        QString path = QString::fromLocal8Bit(shot);
-        QTimer::singleShot(4000, &w, [&w, path]() {
-            w.grab().save(path);
-            QApplication::quit();
-        });
-    }
 
     return app.exec();
 }
